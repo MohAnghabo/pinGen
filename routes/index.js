4fa53f5cff75b3411e9001dac2a3d6c0b76381f1
Mohammed Anghabo <moh.anghabo@gmail.com>
2020-05-16 03:10:56 +0400

    adding unvalid pins

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   let validPins = []
+  let unvalidPins = []
 
   function pinChecker(pin) {
     let pingString = pin.toString().split("");
@@ -29,33 +30,52 @@ router.get('/', function (req, res, next) {
 
   function pinGenerator() {
     let pin = [];
+    let unvalidPin = []
     for (let i = 0; i <= 9999; i++) {
       if (i < 10) {
         if (pinChecker("000" + i) == true) {
           pin.push("000" + i);
+        } else {
+          unvalidPin.push("000" + i);
         }
       } else if (i < 100) {
         if (pinChecker("00" + i) == true) {
           pin.push("00" + i);
+        } else {
+          unvalidPin.push("00" + i);
         }
       } else if (i < 1000) {
         if (pinChecker("0" + i) == true) {
           pin.push("0" + i);
+        } else {
+          unvalidPin.push("0" + i);
         }
       } else {
         if (pinChecker(i) == true) {
           pin.push(i);
+        } else {
+          unvalidPin.push(i);
         }
       }
     }
-    return pin;
+    return {
+      validPins: pin,
+      unvalidPins: unvalidPin
+    };
   }
 
-  validPins = pinGenerator();
+  validPins = pinGenerator().validPins;
   let validPinsCount = validPins.length;
+
+  unvalidPins = pinGenerator().unvalidPins;
+  let unvalidPinsCount = unvalidPins.length;
+
+
   res.send({
     validPinsCount: validPinsCount,
-    validPins: pinGenerator(),
+    validPins: pinGenerator().validPins,
+    unvalidPinsCount: unvalidPinsCount,
+    unvalidPins: pinGenerator().unvalidPins,
   });
 });
 
